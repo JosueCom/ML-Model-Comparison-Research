fddc164cfb0787e008da2428b601cfd50a8a6557
Josue N Rivera <josuevaldez1.com@outlook.com>
2019-06-25 13:59:48 -0500

    Added custom model.py

---
+++ Deep Learning/custom model.py
@@ -0,0 +1,17 @@
+from tensorflow.keras import Input, Dense, Flatten, LSTM
+from keras.models import Model, Sequential
+
+input_size = 9
+
+data_input = Input(shape=(input_size,))
+
+hidden = Dense(20, activation="softplus")(data_input)
+hidden = Dense(10, activation="softplus")(hidden)
+hidden = Dropout(0.8)(hidden)
+data_output = Dense(2, activation="sigmoid")(hidden)
+
+model = Model(inputs=data_input, outputs=data_output)
+
+model.compile(metrics=["accuracy"])
+
+model.fit()
