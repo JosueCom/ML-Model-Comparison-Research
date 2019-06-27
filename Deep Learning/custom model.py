393e5645553e695ebf9e07848a9aa6e1a1f82f7a
Josue N Rivera <josuevaldez1.com@outlook.com>
2019-06-27 02:26:23 -0500

    Load and save network
    Code to load and save network was added. "custom model.py" saves a nn, while "load model.py" loads the model back.

--- Deep Learning/custom model.py
+++ Deep Learning/custom model.py
@@ -13,7 +13,7 @@ Y = df.iloc[:, -1].to_numpy()
 
 data_input = Input(shape=(input_size,))
 
-hidden = Dense(20, activation="softmax")(data_input)
+hidden = Dense(20, activation="softplus")(data_input)
 hidden = Dense(10, activation="softmax")(hidden)
 hidden = Dropout(0.2)(hidden)
 data_output = Dense(1, activation="sigmoid")(hidden)
@@ -23,8 +23,12 @@ model = Model(inputs=data_input, outputs=data_output)
 # compile model
 model.compile(optimizer="adam", loss='binary_crossentropy', metrics=[Precision(), "accuracy"])
 
-model.fit(X, Y, epochs=150)
+model.fit(X, Y, epochs=50)
 
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 print("\n%s: %.2f%%" % (model.metrics_names[2], scores[2]*100))
+
+model.summary()
+
+# model.save("custom_model.h5")
---
+++ Deep Learning/load model.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.layers import Input, Dense, Flatten, LSTM, Dropout
+from tensorflow.keras.models import Model, Sequential, load_model
+from tensorflow.keras.metrics import Precision
+import pandas as pd 
+import numpy as np
+
+df = pd.read_csv("../Data Practice/adjusted.csv")
+
+X = df.iloc[:, 1:-1].to_numpy()
+Y = df.iloc[:, -1].to_numpy()
+
+
+model = load_model("custom_model.h5")
+
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print("\n%s: %.2f%%" % (model.metrics_names[2], scores[2]*100))
+
+model.summary()
+
+# model.save("custom_model.h5")
